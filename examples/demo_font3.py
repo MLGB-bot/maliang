c57f9d46728b7b84dd7fc8856be4e21f7af19d82
jisu_be <>
2022-12-15 19:27:15 +0800

    bug-fix

--- examples/demo_font3.py
+++ examples/demo_font3.py
@@ -25,10 +25,11 @@ app.set_static_relative_dir('./resources/')
 
 # f1 = app.load_font("yezigongchangweifengshouji.ttf", engine_id=0, engine=FontEnginePillow)
 # f1 = app.load_font("yezigongchangweifengshouji.ttf", engine_id=app.FONT_PILLOW)
-f1 = app.load_font("yezigongchangweifengshouji.ttf", engine_id=0, engine=FontEnginePillow)
+# f1 = app.load_font("yezigongchangweifengshouji.ttf", engine_id=0, engine=FontEnginePillow)
 f1 = app.load_font("yezigongchangweifengshouji.ttf", engine_id=0, engine=FontEngineFreetype)
 
 text = "hello Fontf 你好\n我好大家好\n叫啊叫啊\n" + "FONT, 你好sjf坏大hi我的\n安怀海端是阿汉地\n的赛的话uida"
+# text = "hello \n Fontf"
 
 image = app.text_image(text, font=f1)
 
@@ -39,12 +40,12 @@ def on_setup():
     # text = "1asdad"
     # f1 = app.load_font("yezigongchangguojiangti.ttf", font_engine=app.FONT_PILLOW)
     # f1.text_image(text, text_size=12, text_color=(0,0,255, ), )
-    # app.text(text, 0 , 0 , text_size=12, text_color=(0,0,255, ), font=f1)
+    app.text(text, 0 , 0 , text_size=12, text_color=(0,0,255, ), font=f1)
     # app.text(text, 0 , 50 , text_size=18, text_color=(0,0,255, ), font=f1)
     # app.text(text, 0 , 100 , text_size=24, text_color=(0,0,255, ), font=f1)
     # app.text(text, 0 , 150 , text_size=36, text_color=(0,0,255, ), font=f1)
-    app.text(text, 0 , 200 , text_size=48, text_color=(0,0,255, ), font=f1)
-    app.text(text, 0 , 200 , text_size=48, text_color=(0, 255, 0, 100), font=f1)
+    # app.text(text, 0 , 200 , text_size=48, text_color=(0,0,255, ), font=f1)
+    # app.text(text, 0 , 200 , text_size=48, text_color=(0, 255, 0, 100), font=f1)
     app.line(0, app.height/2, app.width, app.height/2)
     # app.text(text, 0, 200, text_size=24, text_color=(0,0,255, ), font=None, space=10)
 
@@ -61,7 +62,7 @@ def on_draw():
     pr.draw_fps(20, app.height-30)
 
 app.regist_event('on_setup', on_setup)
-app.regist_event('on_draw', on_draw)
+# app.regist_event('on_draw', on_draw)
 
 app.loop()
 # pr.unload_font(font)
