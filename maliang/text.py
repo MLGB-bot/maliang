f8fb4d8611a5a4b4b26c112c663d7931c0a44e8a
jisu_be <>
2022-12-17 17:31:19 +0800

    sapce

--- maliang/text.py
+++ maliang/text.py
@@ -20,23 +20,21 @@ class Text:
         self.text_font = font
 
     def text(self, text, x=0, y=0, text_size=None, text_color=None, font: MFont = None):
-        space = 0   # not supported yet
         font = font or self.text_font
         text_size = text_size or self.text_size
         text_color = text_color or self.text_color
         if font:
-            font.text(text, x, y, text_size=text_size, text_color=tuple(MColor(*text_color)))
+            font.text(text, x, y, text_size=text_size, text_color=tuple(MColor(*text_color)), space_x=1)
         else:
-            pr.draw_text_ex(pr.get_font_default(), text, pr.Vector2(x, y), text_size, space, MColor(*text_color).to_pyray())
+            pr.draw_text_ex(pr.get_font_default(), text, pr.Vector2(x, y), text_size, 1, MColor(*text_color).to_pyray())
 
     def text_image(self, text, text_size=12, text_color=None, font: MFont = None):
-        space = 0   # not supported yet
         font = font or self.text_font
         text_size = text_size or self.text_size
         text_color = text_color or self.text_color
         if font:
-            img = font.text_image(text, text_size=text_size, text_color=tuple(MColor(*text_color)))
+            img = font.text_image(text, text_size=text_size, text_color=tuple(MColor(*text_color)), space_x=1)
         else:
             img = MImage()
-            img.pr_image = pr.image_text_ex(pr.get_font_default(), text, text_size, space, MColor(*text_color).to_pyray())
+            img.pr_image = pr.image_text_ex(pr.get_font_default(), text, text_size, 1, MColor(*text_color).to_pyray())
         return img
