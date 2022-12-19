38f26377c83c7bfa06c2e2cfcec3f7cedfbcbbf1
jisu_be <>
2022-12-19 18:40:12 +0800

    add camera3d demo

---
+++ examples/demo_camera3d2.py
@@ -0,0 +1,54 @@
+# -*- coding:utf-8 -*-
+import random
+from maliang import Maliang
+from maliang.units import *
+import pyray as pr
+
+app = Maliang(width=800, height=450, double_buffer=True, fps=80)
+app.set_trace_log_level(4)
+
+MAX_BUILDINGS = 1000
+
+
+camera = app.camera_3d()
+
+camera.position = (10, 10, 10)
+camera.target = (0, 0, 0)
+camera.up = (0, 1, 0)
+camera.fovy = 45.0
+camera.projection = app.CAMERA_PROJECTION_PERSPECTIVE
+
+camera.set_mode(CameraMode.CAMERA_FREE)
+
+cube_position = (0, 0, 0)
+
+# camera.target = (player['x'] + 20.0, player['y'] + 20.0, )
+# camera.offset = (app.width/2, app.height/2)
+# camera.rotation = 0
+# camera.zoom = 1.0
+RED = (255, 0, 0)
+BLACK = (0, 0 ,0)
+DARKGRAY = (80, 80, 80)
+
+def on_setup():
+    # app.no_stroke()
+    pass
+
+def on_draw():
+    camera.update()
+    if app.is_key_pressed(KeyboardKeys.KEY_Z):
+        camera.target = (0, 0, 0)
+
+    app.background(*pr.RAYWHITE)
+
+    app.begin_camera(camera)
+    pr.draw_cube(pr.Vector3(*cube_position), 2, 2,2, pr.RED)
+    # app.cube(*cube_position, 2, 2, 2, filled_color=RED, stroke_width=1, stroke_color=BLACK)
+    app.grid(10, 1)
+    app.end_camera()
+
+
+app.regist_event('on_setup', on_setup)
+app.regist_event('on_draw', on_draw)
+
+app.loop()
