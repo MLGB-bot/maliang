a170b2a2801b98a951aad7c858c3d14ecfa0d713
jisu_be <>
2023-01-10 19:31:08 +0800

    add ellipse line

--- maliang/shape_conf.py
+++ maliang/shape_conf.py
@@ -1,4 +1,7 @@
+import math
+
 import pyray as pr
+import raylib as rl
 from maliang.structs import MColor
 
 
@@ -94,3 +97,55 @@ class ShapeConfig(object):
             area += point[0] * points[neighboor_index][1] - point[1] * points[neighboor_index][0]
         return area * 0.5
 
+    @classmethod
+    def ellipse_coor_by_degree(cls, x, y, rx, ry, degree):
+        # DrawTriangleStrip(strip, 4, color);
+        return (x + math.cos(math.radians(degree)) * rx,
+                y + math.sin(math.radians(degree)) * ry)
+
+
+    @classmethod
+    def draw_ellipse_line_py(cls, x, y, rx, ry, stroke_width, color):
+        points0 = []  # 0~90
+        points1 = []  # 90~180
+        points2 = []  # 180~270
+        points3 = []  # 270~360
+
+        for degree in range(0, 90, 10):
+            _x, _y = cls.ellipse_coor_by_degree(x, y, rx, ry, degree)
+            points0.append((_x, _y))
+            points1.append((2 * x - _x, _y))
+            if degree > 0:
+                points2.append((2 * x - _x, 2 * y - _y))
+            points3.append((_x, 2 * y - _y))
+
+        _x, _y = cls.ellipse_coor_by_degree(x, y, rx, ry, 90)
+        points0.append((_x, _y))
+        points2.append((2 * x - _x, 2 * y - _y))
+
+        points1.reverse()
+        points3.reverse()
+
+        points = [
+            *points0,
+            *points1,
+            *points2,
+            *points3
+        ]
+
+        stripes = []
+        for i in range(0, len(points) - 1):
+            point = points[i]
+            next_point = points[i + 1]
+            delta = (next_point[0] - point[0], next_point[1] - point[1])
+            length = math.dist(point, next_point)
+            scale = stroke_width / (2 * length)
+            radius = -scale * delta[1], scale * delta[0]
+            stripes.append((point[0] - radius[0], point[1] - radius[1]))
+            stripes.append((point[0] + radius[0], point[1] + radius[1]))
+            stripes.append((next_point[0] - radius[0], next_point[1] - radius[1]))
+            stripes.append((next_point[0] + radius[0], next_point[1] + radius[1]))
+        rl.DrawTriangleStrip(stripes, len(stripes), color)
+
+if __name__ == '__main__':
+    print(ShapeConfig.ellipse_coor_by_degree(50, 50, 40, 30, 0))
